56bf788c4c36224e05f5360902cb270b907e02ac
Sergio M <serg0971@easv365.dk>
2023-05-18 13:31:51 +0200

    Finally managed to not shit myself :)

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(20, "John");
+            person.SayHello();
+        }
+    }
+}
