03734c0c3d5a22957094a4a3975477d14de17e37
Sergio M <serg0971@easv365.dk>
2023-05-19 19:56:47 +0200

    Compiler files + changes in Program

--- randomnumber/Program.cs
+++ randomnumber/Program.cs
@@ -3,6 +3,40 @@ class Program
 {
     static void Main(string[] args)
     {
+        var cajon = new Cajon();
 
+        var numberOfRNG = Console.ReadLine();
+        List<String> nombres = new List<String>();
+
+        Console.WriteLine("¡Hola! Bienvenido a la caja de números aleatorios");
+        Console.WriteLine("¿Cuántos números aleatorios quieres?");
+
+        for (int i = 0; i < Convert.ToInt32(numberOfRNG); i++)
+        {
+            Console.WriteLine($"¿Cómo quieres llamar al generador de números aleatorios {i + 1}?");
+            var name = Console.ReadLine();
+            
+            nombres.Add(name);
+        }
+
+        Console.WriteLine("¡Genial! Ahora, ¿cuántos números quieres sacar?");
+
+        var numberToTakeOut = Console.ReadLine();
+
+        for (int i = 0; i < Convert.ToInt32(numberToTakeOut); i++)
+        {
+            var randomNumber = cajon.takeOutRandomNumberGenerator();
+            Console.WriteLine($"Número aleatorio: {randomNumber}");
+        }
+
+        Console.WriteLine("¡Gracias por usar la caja de números aleatorios!");
+
+        waitForKeyPress();
+    }
+
+    private static void waitForKeyPress()
+    {
+        Console.WriteLine("Presiona cualquier tecla para salir...");
+        Console.ReadKey();
     }
 }
