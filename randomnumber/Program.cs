994119965e3d2b2904c533c2df72caa77d6b868e
Sergio M <serg0971@easv365.dk>
2023-05-19 20:01:41 +0200

    Update Program.cs

--- randomnumber/Program.cs
+++ randomnumber/Program.cs
@@ -9,21 +9,25 @@ class Program
         List<String> nombres = new List<String>();
 
         Console.WriteLine("¡Hola! Bienvenido a la caja de números aleatorios");
-        Console.WriteLine("¿Cuántos números aleatorios quieres?");
+        Console.WriteLine("¿Cuántos generadores de números aleatorios quieres?");
 
         for (int i = 0; i < Convert.ToInt32(numberOfRNG); i++)
         {
             Console.WriteLine($"¿Cómo quieres llamar al generador de números aleatorios {i + 1}?");
             var name = Console.ReadLine();
-            
+            while(name == "" || nombres.Contains(name)){
+                Console.WriteLine("El nombre no puede estar vacío o repetido");
+                Console.WriteLine($"¿Cómo quieres llamar al generador de números aleatorios {i + 1}?");
+                name = Console.ReadLine();
+            }
             nombres.Add(name);
         }
 
         Console.WriteLine("¡Genial! Ahora, ¿cuántos números quieres sacar?");
 
-        var numberToTakeOut = Console.ReadLine();
+        var numberOfRGNToTakeOut = Console.ReadLine();
 
-        for (int i = 0; i < Convert.ToInt32(numberToTakeOut); i++)
+        for (int i = 0; i < Convert.ToInt32(numberOfRGNToTakeOut); i++)
         {
             var randomNumber = cajon.takeOutRandomNumberGenerator();
             Console.WriteLine($"Número aleatorio: {randomNumber}");
