073f36c1bc7b0537927070a4f8461cb932edb8d5
tch0u <ignacioaguilarfdez@gmail.com>
2023-05-20 19:47:41 +0200

    rules that i forgot

--- randomnumber/Program.cs
+++ randomnumber/Program.cs
@@ -1,8 +1,12 @@
-﻿namespace randomnumber;
+﻿//El usuario le da nombre a 5 RNG y el programa los introducirá en el Cajon.
+//Tras haberlos introducido, el usuario introducirá un número del 1 al 5 y sacará el número que haya introducido de RNG.
+//El programa printea el número que los RNG hayan reproducido al sacarlos del cajón.
+
+namespace randomnumber;
 class Program
 {
     static void Main(string[] args)
     {
-
+        
     }
 }
